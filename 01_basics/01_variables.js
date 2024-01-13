9fa6a6024f25270e22e2a8c0de61f4e9c2df12e6
M Sajid Hussain <53912407+SaJid96@users.noreply.github.com>
2024-01-13 12:12:39 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID=33221;
+let accountEmail="sajid@gmail.com";
+var accountPassword="jaipur";
+accountCity="Bengaluru";
+let accountState;
+
+
+console.log(accountID,accountEmail,accountPassword,accountCity,accountState);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
