c094116a03456af1717a6639fdc27e28447d5605
M Sajid Hussain <53912407+SaJid96@users.noreply.github.com>
2024-01-13 17:45:26 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,9 @@ var accountPassword="jaipur";
 accountCity="Bengaluru";
 let accountState;
 
-
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.log(accountID,accountEmail,accountPassword,accountCity,accountState);
 console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
